bf2d089d45a8d01349932ea0c029971a605b6059
enockyang <enock0504@utexas.edu>
2018-03-28 17:08:05 -0500

    3

--- practice.py
+++ practice.py
@@ -1,9 +1,2 @@
-def index_words_new(text):
-    if text:
-        yield 0
-    for index, letter in enumerate(text,1):
-        if letter == ' ':
-            yield index
 
-for x in index_words_new('i am a good person'):
-    print(x)
+
